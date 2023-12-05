3e4cf005a38cb68003a77d2850df858cb621caad
JokerMagic2333 <jokermagic2333@163.com>
2023-12-05 09:34:19 +0800

    修正打包命令

--- 打包命令.bat
+++ 打包命令.bat
@@ -1,5 +1,5 @@
 chcp 65001
-Pyinstaller -F -w %~dp0\main.py -n 假期倒计时 -i %~dp0\logo.ico
-COPY /Y %~dp0\cfg.ini %~dp0\dist\
-COPY /Y %~dp0\设置开机自启动.bat %~dp0\dist\
-COPY /Y %~dp0\取消开机自启动.bat %~dp0\dist\
+Pyinstaller -F -w %~dp0main.py -n 假期倒计时 -i %~dp0logo.ico
+COPY /Y %~dp0cfg.ini %~dp0dist\
+COPY /Y %~dp0设置开机自启动.bat %~dp0dist\
+COPY /Y %~dp0取消开机自启动.bat %~dp0dist\
